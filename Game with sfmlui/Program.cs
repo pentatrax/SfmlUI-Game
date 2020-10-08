0e26cf7192da6832f16edb58b7c477ec90f08238
PentaTrax <philipnordnielsen@gmail.com>
2020-10-08 08:45:47 +0200

    Add files via upload

---
+++ Game with sfmlui/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using SfmlUI;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Game_with_sfmlui
+{
+    class Program
+    {
+        public enum State {Menu, Settings, Play, Pause, GameOver}
+        static void Main(string[] args)
+        {
+            RenderWindow Window = new RenderWindow(VideoMode.FullscreenModes[0], "SfmlUI Game", Styles.Fullscreen);
+            Window.Closed += CloseGame;
+            Font GlobalFont = new Font("8-bit Arcade In.ttf");
+
+            State _state = State.Menu;
+
+            Menu menu = new Menu(Window, GlobalFont);
+            menu.StateShiftToPlay += StateToPlay;
+            menu.StateShiftToSettings += StateToSettings;
+
+            Settings settings = new Settings(Window, GlobalFont);
+
+
+            while (Window.IsOpen)
+            {
+                Window.Clear();
+                Window.DispatchEvents();
+                switch (_state)
+                {
+                    case State.Menu: menu.Draw(); break;
+                    case State.Settings: settings.Draw(); break;
+                    case State.Play: break;
+                    case State.Pause: break;
+                    case State.GameOver: break;
+                }
+                Window.Display();
+                
+            }
+
+            void CloseGame(object sender, EventArgs e)
+            {
+                Window.Close();
+            }
+
+            void StateToPlay(object sender, EventArgs e)
+            {
+                _state = State.Play;
+                Console.WriteLine(_state);
+            }
+            void StateToSettings(object sender, EventArgs e)
+            {
+                _state = State.Settings;
+                Console.WriteLine(_state);
+            }
+            void StateToPause(object sender, EventArgs e)
+            {
+                _state = State.Pause;
+            }
+            void StateToGameOver(object sender, EventArgs e)
+            {
+                _state = State.GameOver;
+            }
+        }
+    }
+}
---
+++ Game with sfmlui/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SfmlUI;
+using SFML;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Game_with_sfmlui
+{
+    class Settings
+    {
+        private RenderWindow _window;
+        private Dropdown _resolutionPicker;
+        private Button _back;
+        private Checkbox _fullscreen;
+        public event EventHandler StateShiftToMenu;
+
+        public Settings(RenderWindow window, Font font)
+        {
+            _window = window;
+            
+            _resolutionPicker = new Dropdown(_window, new Vector2f(window.Size.X * 0.33f, window.Size.Y * 0.33f), font, 20, "");
+            
+
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
