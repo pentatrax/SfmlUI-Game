33533c5f8126833d57a552be5382f6c59b7c499a
PentaTrax <philipnordnielsen@gmai..com>
2020-10-20 15:04:47 +0200

    more code to the player class

---
+++ Game with sfmlui/Bodypart.cs
@@ -0,0 +1,54 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_with_sfmlui
+{
+    class Bodypart
+    {
+        public enum Type { Head, Limb, Torso }
+        private RenderWindow _window;
+        private int _zIndex = 0;
+        private Vector2f _link;
+        private Type _type;
+        private Sprite _sprite;
+
+        public int zIndex { get { return _zIndex; } set { _zIndex = value; } }
+        public float Rotation { get { return _sprite.Rotation; } set { _sprite.Rotation = value; } }
+        public Vector2f Link { get { return _link; } set { _link = value; _sprite.Position = value; } }
+
+        public Bodypart(RenderWindow window, Vector2f link, Type type, Sprite sprite)
+        {
+            _window = window;
+            _link = link;
+            _type = type;
+            _sprite = sprite;
+            switch (type)
+            {
+                case Type.Limb:
+                    _sprite.Origin = new Vector2f(sprite.GetGlobalBounds().Width * 0.5f, sprite.GetGlobalBounds().Height * 0.05f);
+                    break;
+                case Type.Head:
+                    _sprite.Origin = new Vector2f(sprite.GetGlobalBounds().Width * 0.5f, sprite.GetGlobalBounds().Height * 0.95f);
+                    break;
+                case Type.Torso:
+                    _sprite.Origin = new Vector2f(sprite.GetGlobalBounds().Width * 0.5f, sprite.GetGlobalBounds().Height);
+                    break;
+            }
+            _sprite.Position = link;
+
+        }
+
+        public void Draw()
+        {
+            _window.Draw(_sprite);
+        }
+
+        public void Rotate(float angle)
+        {
+            _sprite.Transform.Rotate(angle);
+        }
+    }
+}
--- Game with sfmlui/Player.cs
+++ Game with sfmlui/Player.cs
@@ -1,5 +1,6 @@
 ﻿using SFML.Graphics;
 using SFML.System;
+using SfmlUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,13 +19,46 @@ namespace Game_with_sfmlui
         private Vector2f _acceleration;
 
         // Player parts
+        private List<Bodypart> _bodyParts = new List<Bodypart>();
         private Text name;
-        private Limb _head;
-        private Limb _leftArm;
-        private Limb _rightArm;
-        private Limb _leftLeg;
-        private Limb _rightLeg;
 
+        public Player(RenderWindow window, Font font, Vector2f position, string name, Sprite playerSpriteSheet)
+        {
+            _bodyParts.Add(new Bodypart(window, position, Bodypart.Type.Torso, playerSpriteSheet));
+
+        }
+
+        void Draw()
+        {
+            _bodyParts = SortBodyPartsByIndex_Z(_bodyParts);
+            foreach (Bodypart part in _bodyParts)
+            {
+                part.Draw();
+            }
+        }
+
+        List<Bodypart> SortBodyPartsByIndex_Z(List<Bodypart> list)
+        {
+            List<Bodypart> tempList = new List<Bodypart>();
+            int tempCounter = 0;
+
+            for (int i=0; i<list.Count; i++)
+            {
+                if (list[i].zIndex == tempCounter)
+                {
+                    tempList.Add(list[i]);
+                    list.RemoveAt(i);
+                }
+
+                if (list.Count > 0 && i == list.Count - 1)
+                {
+                    i = 0;
+                    tempCounter++;
+                }
+            }
+
+            return tempList;
+        }
 
     }
 }
